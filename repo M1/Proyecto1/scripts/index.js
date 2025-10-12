896d5c5c05de268852f49c6485490c9bb67c4c95
Elernes25 <ernesto.roura@gmail.com>
2025-10-12 19:15:54 -0300

    clases creadas

--- repo M1/Proyecto1/scripts/index.js
+++ repo M1/Proyecto1/scripts/index.js
@@ -1,35 +1,57 @@
 console.log("estoy conectado");
 
 //alert("ventana emergente")
-class Activity {
+class Diseño{         //diseño para mostrar y elegir
     constructor(id, nombre, descripcion, enlace){
         this.id=id;   //acumular (numero de orden detarjetas)
         this.nombre=nombre;
         this.descripcion=descripcion;
         this.enlace=enlace;
     }
-
 }
 
 class Repositorio{
     constructor(){
-        this.actividades=[]; //array de objetos
+        this.diseños=[]; //array de objetos (Propiedad activities => Un arreglo para almacenar los distintos diseños.)
         this.cont=1; //contador
     }
-    traeTodasLasActividades(){
-        return this.actividades; //devuelve el array con todos los objetos cargados
+    traeTodosLosDiseños(){
+        return this.diseños; //devuelve el array con todos los objetos/diseños cargados
     }
-    crearActividad(nom,desc,enlace){
-        const objAct=new Activity(this.contador,nom,desc,enlace); //objeto que va a l array
-        this.actividades.push();
-        this.contador++;
+    crearDiseño(nom,desc,enlace){
+        let objAct=new Diseño(this.cont,nom,desc,enlace); //objeto que va a l array
+        this.diseños.push(objAct);
+        this.cont++;
         return "Creado";
-
+    }
+    borrarDiseño(id){ //=> Debe recibir un id y filtrar el arreglo para eliminar la actividad correspondiente.
+        let arrDiseñosLimpio=this.diseños.filter(objeto=>objeto.id!==id); //el arrego contiene objetos
+        if (arrDiseñosLimpio!==""){  //si encontró el elemento
+            this.diseños=arrDiseñosLimpio; //modifico el array 
+        }               
+        return this.diseños;
     }
 }
-let repositorio =new Repositorio();
 
-//
+let repositorio =new Repositorio();
+//**************PRUEBAS ****************/
+repositorio.crearDiseño("vidriera1", "temporada octubre","enlace URL imagen1");
+console.log(repositorio.diseños[0]);
+repositorio.crearDiseño("pared1", "ambiente cool","enlace URL imagen2");
+console.log(repositorio.diseños[1]);
+repositorio.crearDiseño("auto1", "racingspeedcool","enlace URL imagen3");
+console.log(repositorio.diseños[2]);
+repositorio.crearDiseño("vidriera2", "navidad01","enlace URL imagen4");
+console.log(repositorio.diseños[3]);
+
+console.log(repositorio.diseños);
+
+repositorio.borrarDiseño(3);
+console.log(repositorio.diseños);
+console.log(repositorio.traeTodosLosDiseños());
+//**************FIN PRUEBAS ****************/
+
+//**************DOM AUN NO FUNCIONA************************** */
 const botonE=documents.getElementsByID("botonEnviar");
 //botonE.addEventListener("click",()=>{}); //dblclick//
 
